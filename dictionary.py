e01dd1b8c74dc799ba81341ce8ebc18b43e15bf0
Admin <sbnyuval@gmail.com>
2026-08-20 10:21:23 +0300

    YUVAL

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,12 @@
-dic = {"grade" : "10" "11" "12",
-       "class" : "math" "english" "history" "literature" "language" "bible" "citizenship",
-       "math_topic" : "algebra" "geometry",
-       "english_topic" : "the wave" "the enemy",
-       "history_topic" : "World War I" "World War II",
-       "literature_topic" : "tehila" "the lady and the peddler",
-       "language_topic" : "number name" "reading comprehension",
-       "bible_topic" : "tora" "nevim",
-       "citizenship_topic" : "legal rights" "the three authorities"
+import pandas as pd
+dic = {"grade" : ["10", "11", "12"],
+       "class" : ["math" "english", "history" "literature", "language", "bible", "citizenship"],
+       "math_topic" : ["algebra", "geometry"],
+       "english_topic" : ["the wave", "the enemy"],
+       "history_topic" : ["World War I", "World War II"],
+       "literature_topic" : ["tehila", "the lady and the peddler"],
+       "language_topic" : ["number name", "reading comprehension"],
+       "bible_topic" : ["tora", "nevim"],
+       "citizenship_topic" : ["legal rights", "the three authorities"]
        }
+
