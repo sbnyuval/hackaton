20b3aaf2a76e254b74730ea6356d03e8b3744cbe
yaelih2110 <yaelih2110@gmail.com>
2026-08-20 11:18:35 +0300

    commit_staff_done

--- STAFF.py
+++ STAFF.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 import dictionary
 from menufunc import menu
-def enter_new_teachers_to_school_system():
+STAFF_JOB = []
+JOBS_LIST=['teacher','counselor','maintenance']
+num_of_School_staff=['0','1','2','3']
+def enter_new_teachers_to_school_system(STAFF_JOB,num_of_school_staff, JOBS_LIST):
+    # רשימה שמכילה כמות מקסימום של מורים בבית ספר
     num_of_School_staff=[]
     for i in range(200):
         num_of_School_staff.append(str(i+1))
+    # הכנסת מידע על ידי משתמש- בדיקה שהערך הוא בטווח הרצוי והרצת לולאה שרצה לפי מספר המורים שרוצים להוסיף
     num_staff="Enter number of School staff: "
     num=menu(num_staff)
     if num in num_of_School_staff:
@@ -13,19 +18,25 @@ def enter_new_teachers_to_school_system():
         while num not in num_of_School_staff:
             if num == 'exit':
                 break
-            num = menufunc.menu(num_staff)
+            num = menu(num_staff)
 
-    STAFF_NAMES_AND_JOBS={}
+    STAFF_NAMES_AND_JOBS=[]
     STAFF_GRADES=[]
     STAFF_JOB=[]
-    staff_name="Enter STAFF NAME: "
-    job_name="Enter JOB NAME: "
+    staff_name="Enter new staff name: "
+    job_name="Enter job name: "
     num=int(num)
     for i in range(num):
         user=menu(staff_name)
-        STAFF_NAMES_AND_JOBS[i]=user
-        STAFF_JOB = []
+        STAFF_NAMES_AND_JOBS.append(user)
         JOB=menu(job_name)
+        if JOB.lower in JOBS_LIST:
+            pass
+        else:
+            while JOB.lower not in JOBS_LIST:
+                if JOB.lower == 'exit':
+                    break
+                JOB = menu(job_name)
         possible_grades_per_teacher=['0','1','2','3']
         num_of_grades_per_teacher="Enter number of Grades: "
         NUM_OF_STAFF_GRADES=menu(num_of_grades_per_teacher)
@@ -36,7 +47,7 @@ def enter_new_teachers_to_school_system():
             while NUM_OF_STAFF_GRADES not in possible_grades_per_teacher:
                 if NUM_OF_STAFF_GRADES == 'exit':
                     break
-                NUM_OF_STAFF_GRADES = menufunc.menu(grades)
+                NUM_OF_STAFF_GRADES = menu(grades)
         NUM_OF_STAFF_GRADES=int(NUM_OF_STAFF_GRADES)
         if NUM_OF_STAFF_GRADES!=0:
             for j in range(NUM_OF_STAFF_GRADES):
@@ -47,8 +58,9 @@ def enter_new_teachers_to_school_system():
                     while GRADES in dictionary.dic[1]:
                         if GRADES== 'exit':
                             break
-                        GRADES = menufunc.menu(grades)
+                        GRADES = menu(grades)
                 GRADES = int(GRADES)
                 STAFF_NAMES_AND_JOBS[i][j]=GRADES
         else:
             continue
+enter_new_teachers_to_school_system(STAFF_JOB,num_of_School_staff,JOBS_LIST)
